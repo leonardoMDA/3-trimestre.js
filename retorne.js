86f484a9524899f28334531ee81027168fcec792
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-09-17 08:49:55 -0300

    Delete retorne.js

--- retorne.js
+++
@@ -1,7 +0,0 @@
-function filtrarPares(arr) {
-    return arr.filter(num => num % 2 === 0);
-}
-
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const pares = filtrarPares(numeros);
-console.log(`Números pares: ${pares}.`)
